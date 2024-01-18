20f6d225277dab483ce7725ca450bd8187761068
nguyend8230 <nguyend8230@gmail.com>
2024-01-18 16:46:00 +0700

    cleaned up code

--- mainwindow.h
+++ mainwindow.h
@@ -52,7 +52,7 @@ private:
     // 2d list to keep track of the number of neighbors a cell has
     QMap<QList<int>,int> neighbors_count;
 
-    // list of cells that needs to be flipped at the start of the next iteration of simulate()
+    // set of cells that need to be flipped at the start of the next iteration of simulate()
     QSet<QList<int>> flipped_cells;
 
     // -----FUNCTIONS-----
